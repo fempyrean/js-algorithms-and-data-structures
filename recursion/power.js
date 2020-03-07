c13dfcabb6f0da410375a1ead18530cced6c59a8
Lucas Sousa <37604840+fempyrean@users.noreply.github.com>
2020-03-07 11:36:21 -0300

    Recursve power
    Simple implementation of power using recursion.

--- recursion/power.js
+++ recursion/power.js
@@ -1,2 +1,9 @@
 const base = Number(process.argv[2]) || 10;
 const exponent = Number(process.argv[2]) || 10;
+
+/** Recursive and simple implementation of power */
+function power(base, exponent) {
+  if (base <= 0) return 0;
+  if (exponent == 0) return 1;
+  return base * power(base, exponent - 1);
+};

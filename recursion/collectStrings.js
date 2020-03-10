b1f1a088f9b6cac6f05223ed72a363b8432015f5
Lucas Sousa <frempyrean@gmail.com>
2020-03-10 19:42:10 -0300

    collectStrings

---
+++ recursion/collectStrings.js
@@ -0,0 +1,40 @@
+/**
+ * Write a function called collectStrings which accepts an object and returns an array
+ * of all the values in the object that have a typeof string.
+ */
+function collectStrings(obj) {
+    let strings = [];
+
+    function helper(input) {
+        Object.keys(input).map(k => {
+            if (typeof input[k] === 'string') {
+                strings.push(input[k]);
+            }
+            if (typeof input[k] === 'object') {
+                return helper(input[k]);
+            }
+        });
+    }
+
+    helper(obj);
+    console.log(strings);
+    return strings;
+}
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
